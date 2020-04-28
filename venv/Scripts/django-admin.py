3c9c0b0ff9dab697cf199b1546015e7d4b8f7635
Marvin Ulanday <marvinulanday@gmail.com>
2020-04-28 20:42:53 +0200

    PATCH quitado

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Marvin\PycharmProjects\Cycling_Backend\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
